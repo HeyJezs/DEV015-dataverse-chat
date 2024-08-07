73c771c5ffe5ef13ff5cf9250a5cf285d2acfd5c
Jessika Sanchez <jessikasanchez@MacBook-Pro-de-Jessika.local>
2024-08-06 19:48:46 -0500

    mitad hito1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-// En este archivo definirás tus rutas e importarás los componentes que vas a renderizar.
+import { setRootEl, setRoutes, onURLChange } from './router.js';
+import Home from './views/Home.js';
+import ChatGroup from './views/ChatGroup.js';
+import ErrorView from './views/error.js';
 
-/*
-import Example from './views/Example.js';
-
-Ejemplo de definición de rutas:
-
-const routes = {
-    "/": Example,
-    ...
-}
-*/
+// Establecer el elemento raíz
+setRootEl(document.getElementById('root'));
+// Definir las rutas y sus vistas correspondientes
+setRoutes({
+    '/': Home,
+    '/chatgroup': Chatgroup,
+    '/error': ErrorView
+});
+// Manejar la navegación mediante clics en los enlaces
+document.addEventListener('click', e => {
+    if (e.target.matches('[data-link]')) {
+        e.preventDefault();
+        window.history.pushState(null, '', e.target.href);
+        onURLChange();
+    }
+});
+// Manejar el cambio de URL al retroceder o avanzar en el historial
+window.addEventListener('popstate', onURLChange);
+// Cargar la vista correcta al inicio
+onURLChange();
 
 /*
 TODO:
