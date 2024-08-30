bf77a9442e7f8a35dc245366975f89ac18c4b94b
Jessika Sanchez <jessikasanchez@MacBook-Pro-de-Jessika.local>
2024-08-30 01:15:32 -0500

    Menu hamburguesa reparado

--- src/index.js
+++ src/index.js
@@ -31,3 +31,25 @@ window.addEventListener('popstate', onURLChange);
 
 // Cargar la vista correcta al inicio
 onURLChange();
+
+// Lógica del menú de hamburguesa
+initHamburgerMenu();
+
+function initHamburgerMenu() {
+  const barsMenu = document.querySelector(".bars-menu");
+  const line1_bars = document.querySelector(".line1-bars-menu");
+  const line2_bars = document.querySelector(".line2-bars-menu");
+  const line3_bars = document.querySelector(".line3-bars-menu");
+  const container_menu = document.querySelector(".menu");
+  const overlay = document.querySelector(".overlay");
+  
+  if (barsMenu) {
+    barsMenu.addEventListener("click", function() {
+      line1_bars.classList.toggle("activeline1-bars-menu");
+      line2_bars.classList.toggle("activeline2-bars-menu");
+      line3_bars.classList.toggle("activeline3-bars-menu");
+      container_menu.classList.toggle("menu_active");
+      overlay.classList.toggle("overlay_active");
+    });
+  }
+}
